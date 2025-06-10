9ab73b993025634641f56df3a241213191cfd400
Edoardo-levi <edoardolevi05@gmail.com>
2025-06-10 20:49:12 +0200

    Università

---
+++ Progettazione 1/Università 1/universita.py
@@ -0,0 +1,19 @@
+from mytipes import *
+
+class facolta:
+    def __init__(self, nome:str, tipologia:str)->None:
+        self.nome=nome
+        self.tipologia= tipologia
+
+class Corso:
+    def __init__(self, nome:str, codice:str, durata:int):
+        self.nome=nome
+        self.codice=codice
+        self.durata=durata
+
+
+# La classe Studente è stata importata da mytipes
+# La classe Professore è stata importata da mytipes
+# La classe Città è stata importata da mytipes
+# La classe Regione è stata importata da mytipes
+# La classe Nazione è stata importata da mytipes
