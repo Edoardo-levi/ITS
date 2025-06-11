d365f72146ea2eecc04a8fc2c785e51e59967975
Edoardo-levi <edoardolevi05@gmail.com>
2025-06-11 13:46:07 +0200

    web 2

--- Web/Web 2/Esercizi/script.jsx
+++ Web/Web 2/Esercizi/script.jsx
@@ -2,14 +2,32 @@ const rootElement= document.querySelector("#root");
 
 const root = ReactDOM.createRoot(rootElement)
 
-const App= () => {
+const App= (props) => {
     return(
         <main className="main" id="main">
             <h1>Primo Componente</h1>
+            {props.children}
         </main>
+    )
+}
+const List=()=>{
+    return (
+        <ul>
+            <li>Php</li>
+            <li>JS</li>
+            <li>Pyhton</li>
+        </ul>
     )
 }
 
 root.render(
-    <App></App>
+    <>
+    <App>
+        <h2>
+            Lista competenze
+        </h2>
+        <List></List>
+    </App>
+    
+    </>
 )
