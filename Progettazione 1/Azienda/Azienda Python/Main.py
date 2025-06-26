4a4df09f8b0efb0c13fc9fd5915365e320aa0f73
Edoardo-levi <edoardolevi05@gmail.com>
2025-06-26 16:02:38 +0200

    Azienda

--- Progettazione 1/Azienda/Azienda Python/Main.py
+++ Progettazione 1/Azienda/Azienda Python/Main.py
@@ -1,39 +1,82 @@
-from mytipes import *
-from Impiegato import *
-from Dipartimento import *
-from Progetto import *
 from datetime import date
+from mytipes import RealeMaggioreDiZero
+from Impiegato import Impiegato
+
+imp = Impiegato("Mario", "Rossi", date(1990, 1, 1), RealeMaggioreDiZero(2500.00))
+
+print(imp.nome())               # Mario
+print(imp.cognome())            # Rossi
+print(imp.nascita())            # 1990-01-01
+print(imp.stipendio())          # 2500.00
 
 
 
-tel1: NumeroTelefono = NumeroTelefono("3334445566")
-tel2: NumeroTelefono = NumeroTelefono("3337778899")
-ind: Indirizzo = Indirizzo("Viale Cesare Pavese", "205b",
-                           CAP("00144"))
+from Dipartimento import Dipartimento
+from mytipes import NumeroTelefono, Indirizzo
 
-alice: Impiegato = Impiegato("Alice", "Alessi",
-                             date(year=1990, month=12, day=31),
-                             RealGez(18000))
-print(f"Ho creato l'impiegata {alice.nome()} {alice.cognome()}")
+tel1 = NumeroTelefono("0123456789")
+indirizzo = Indirizzo("Via Roma 1", "Torino", "10100")
+dip = Dipartimento("Informatica", tel1, indirizzo)
 
-bob: Impiegato = Impiegato("Bob", "Burnham",
-                             date(year=1997, month=10, day=11),
-                             RealGez(19000))
-print(f"Ho creato l'impiegato {bob.nome()} {bob.cognome()}")
+print(dip.nome())               # Informatica
+print(dip.indirizzo())          # Indirizzo
+print(dip.telefoni())           # frozenset with tel1
 
 
-dip1: Dipartimento = Dipartimento("Vendite", tel1, ind)
-
-print(f"Ho creato il dipartimento {dip1}")
 
 
-dip2: Dipartimento = Dipartimento("Acquisti", tel2, None)
-print(f"Ho creato il dipartimento {dip2}")
+from Progetto import Progetto
+from mytipes import RealeMaggioreDiZero
 
-t: frozenset[NumeroTelefono] = dip1.telefoni()
+proj = Progetto("Apollo", RealeMaggioreDiZero(100000))
+print(proj.nome())             # Apollo
+print(proj.budget())          # 100000
 
-print("dip1.telefoni() = " + str(dip1.telefoni()))
 
-dip1.add_NumeroTelefono(NumeroTelefono("3481265413"))
 
-print("dip1.telefoni() = " + str(dip1.telefoni()))
+from Coinvolto import Coinvolto
+from Impiegato import Impiegato
+from Progetto import Progetto
+from datetime import date
+from mytipes import RealeMaggioreDiZero, RealeMaggioreDiZero
+
+imp = Impiegato("Lucia", "Verdi", date(1985, 5, 20), RealeMaggioreDiZero(3200))
+proj = Progetto("Pegaso", RealeMaggioreDiZero(50000))
+
+Coinvolto.add(imp, proj,date.today())
+
+print(len(imp.progetti()))      # 1
+print(proj.is_coinvolto(imp))   # True
+
+
+
+from Afferenza import Afferenza
+from Impiegato import Impiegato
+from Dipartimento import Dipartimento
+from datetime import date
+from mytipes import RealeMaggioreDiZero, NumeroTelefono, Indirizzo
+
+imp = Impiegato("Anna", "Bianchi", date(1988, 3, 15), RealeMaggioreDiZero(2800))
+dip = Dipartimento("Fisica", NumeroTelefono("0111234567"), Indirizzo("Via Galileo", "Torino", "10125"))
+
+Afferenza.add(imp, dip, date(2020, 1, 1))
+
+# Se i metodi imp.add_link_afferenza e dip.add_link_afferenza sono implementati, questo test dovrebbe andare
+print("Test Afferenza completato")
+
+
+
+
+
+from Direzione import Direzione
+from Impiegato import Impiegato
+from Dipartimento import Dipartimento
+from datetime import date
+from mytipes import RealeMaggioreDiZero, NumeroTelefono, Indirizzo
+
+imp = Impiegato("Luca", "Neri", date(1980, 7, 10), RealeMaggioreDiZero(4000))
+dip = Dipartimento("Chimica", NumeroTelefono("0109876543"), Indirizzo("Via Mendeleev", "Genova", "16100"))
+
+Direzione.add(imp, dip)
+
+print("Test Direzione completato")
