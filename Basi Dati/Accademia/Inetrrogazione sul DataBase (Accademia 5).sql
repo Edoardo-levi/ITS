2d7bf1210ed9611594297dd985ab7c0bbe3286c8
Edoardo-levi <edoardolevi05@gmail.com>
2025-07-23 10:37:29 +0200

    DataBase Accademia

--- Basi Dati/Accademia/Inetrrogazione sul DataBase (Accademia 5).sql
+++ Basi Dati/Accademia/Inetrrogazione sul DataBase (Accademia 5).sql
@@ -1,31 +1,78 @@
 -- 1
-select wp.nome, wp.inizio, wp.fine
-	from progetto prog, wp
-		where wp.progetto = prog.id
-		and prog.nome = "Pegasus";
+select wp, wp.inizio, wp.fine, wp.nome
+	from wp, progetto p
+		where wp.progetto = p.id
+		and p.nome = "Pegasus";
 
--- 2 
-select distinct pers.id, pers.nome, pers.cognome, pers.posizione
-	from persona pers, progetto prog, attivitaprogetto ap
-		where ap.persona = pers.id
-		and ap.progetto = prog.id
-		and prog.nome = "Pegasus"
-	order by pers.cognome desc;
+-- 2
+select pers.id, pers.nome, pers.cognome, pers.posizione
+	from AttivitaProgetto attprog, progetto prog, persona pers
+		where attprog.progetto = prog.id 
+			and attprog.persona = pers.id
+			and prog.nome = "Pegasus";
 
--- 3
-select distinct pers.nome, pers.cognome, pers.posizione
-	from  persona pers, progetto prog, attivitaprogetto ap1, attivitaprogetto ap2
-		where persona pers, prog.nome = "Pegasus"
-		and ap1.persona = pers.id
-		and ap2.persona = pers.id
-		and ap1.progetto = prog.id
-		and ap2.progetto = prog.id
-		and ap1.id <> ap2.id;			-- il simbolo <> significa diverso
+-- 3 
+
+select pers.id, pers.nome, pers. posizione
+	from AttivitaProgetto attprog1,AttivitaProgetto attprog2, progetto prog, persona pers
+		where attprog1.id <> attprog2.id
+			and attprog1.progetto = attprog2.progetto
+			and attprog1.persona  = attprog2.persona
+			and attprog1.persona  = persona.id
+			and attprog1.progetto = prog.id
+			and prog.nome = "Pegasus";
 
 
--- 8 
-select distinct p.nome, p.cognome
-	from persona p, attivitaprogetto ap, attiivitanonprogettuale anp
-		where ap.giorno = anp.giorno
-		and ap.persona = p.id 
-		and anp.persona = p.id;
+-- 4
+select pers.id, pers.nome, pers.cognome, posizione
+	from Assenza ass, persona pres
+		where ass.persona = pers.id
+			and tipo = "Malattia"
+			and posizione = "Professore Oridinario";
+
+
+-- 5
+select pers.id, pers.nome, pers.cognome, posizione
+	from Assenza ass1, persona pers, Assenza ass2
+		where ass1.persona = pers.id
+		and ass2.persona = pers.id
+		and ass1.id <> ass2.id
+		and ass1.tipo = "Malattia"
+		and ass2.tipo = "Malattia"
+		and pers.posizione = "Professore Oridinario";
+
+
+-- 6
+select pers.id, pers.nome, pers.cognome
+	from persona pers, AttivitaNonProgettuale anp
+		where pers.id = anp.persona
+		and anp.tipo = "Didattica"
+		and pers.posizione = "Ricercatore";
+
+
+-- 7 
+
+select  pers.id, pers.nome, pers.cognome
+	from persona pers, attivitanonprogettuale anp1, attivitanonprogettuale anp2
+		where anp1.id <> anp2.id
+			and anp1.tipo = 'Didattica'
+			and anp2.tipo = 'Didattica'
+			and anp1.persona = pers.id
+			and anp2.persona = pers.id
+			and s.posizione = 'Ricercatore';
+
+-- 8
+select  pers.id, pers.nome, pers.cognome
+	from persona pers, attivitaprogetto attprog, attivitanonprogettuale anp
+		where
+			pers.id = attprog.persona
+			and pers.id = anp.persona
+			and attprog.giorno = anp.giorno;
+
+
+-- 10
+select pers.id, pers.nome, pers.cognome
+	from persona pers, attivitaprogetto ap, assenza a
+		where pers.id = ap.persona
+			and a.persona = pers.id
+			and ap.giorno = a.giorno;
