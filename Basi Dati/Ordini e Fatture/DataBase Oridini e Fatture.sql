7cce1bd614c5173cab15275051e4c057792dde04
Edoardo-levi <edoardolevi05@gmail.com>
2025-08-26 19:25:13 +0200

    DataBase Ordini e Fatture

--- Basi Dati/Ordini e Fatture/DataBase Oridini e Fatture.sql
+++ Basi Dati/Ordini e Fatture/DataBase Oridini e Fatture.sql
@@ -20,7 +20,8 @@ create domain Cap as varchar (5)
 create type StatoOrdine as
 	enum ('In Preparazione', 'Inviato', 'Da Saldare', 'Saldato');
 
-create domain RealTZU as real
+create domain RealTZU as real 
+	-- TZU (Tra Zero e Uno)
 	check(value >=0 and value <=1);
 
 create domain RealGEZ as real
@@ -41,76 +42,96 @@ create domain StringaM as varchar(100);
 
 -- Creazione Tabelle
 
-
 create table Citta (
 
 	nome StringaM not null,
 
-	primary key (nome)
-);
+	primary key (nome),
 
+	foreign key (regione) 
+		references Regione (nome)
+
+);
 create table Regione (
 
 	nome StringaM not null,
 
-	primary key (nome)
-);
+	primary key (nome),
 
+	foreign key (nazione)
+		references Nazione (nome)
+
+);
 create table Nazione (
 
 	nome StringaM not null,
 
 	primary key (nome)
+
 );
-
-
 create table Direttore (
 
 	nome StringaM not null,
 	cognome StringaM not null,
 	cf CodiceFiscale not null,
-	anni_servizio integer not null,
-	data_nascita data_nascita,
+	anni_servizio IntGEZ not null,
+	data_nascita date not null,
 
-	primary key (cf)
+	primary key (cf),
+
+	foreign key citta
+		references Citta (nome)
 
 );
+create table Dipartimento (
 
+	nome StringaM not null,
+	indirizzo Indirizzo not null,
+
+	primary key (nome),
+
+	foreign key direttore
+		references Direttore (cf),
+
+	foreign key citta
+		references Citta (nome)
+);
 create table Fornitore (
 
 	ragione_sociale StringaM not null,
 	partita_iva PartitaIVA not null,
 	indirizzo Indirizzo not null,
 	telefono Telefono not null,
-	email Email not null
+	email Email not null,
 
-	primary key(partita_iva)
+	primary key (partita_iva),
+
+	foreign key citta
+		references Citta (nome),
+	foreign key ordine
+		references Ordine (codice)
 
 );
+create table Ordine (
 
-create table Dipartimetno (
+	data_stipula Date not null,
+	imponibile RealGEZ not null,
+	aliquota RealTZU not null,
+	descrizione StringaM not null,
+	codice IntGEZ not null,
 
-	nome StringaM not null,
-	indirizzo Indirizzo not null,
+	primary key (codice),
 
-	primary key (nome)
+	foreign key dipartimento
+		references Dipartimento (nome),
+	foreign key statoordine
+		references StatoOrdine (nome)
+
 );
-
-
 create table StatoOrdine (
 
 	nome StringaM not null,
+
 	primary key (nome)
-);
-
-create table Ordine (
-
-	data_stipula date not null,
-	imponibile Regione not null,
-	aliquota RealTZU not null,
-	descrizione StringaM not null,
-	codice integer not null,
-
-	primary key (codice)
 
 );
