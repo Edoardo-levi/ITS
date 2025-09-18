66468d4eace1c85d562bb39a626796e2b14c5a68
Edoardo-levi <edoardolevi05@gmail.com>
2025-09-18 13:18:10 +0200

    Python virtuale

--- Progettazione 2/Nuova App/seconda-app/src/App.jsx
+++ Progettazione 2/Nuova App/seconda-app/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserCrud from './UserCrud'
+import Saluto from './Saluto'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <UserCrud></UserCrud>
+     <Saluto></Saluto>
     </>
   )
 }
